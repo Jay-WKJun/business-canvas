8cc36623a0cb64b8738a53b94508c6529af2165e
KyungJunWoo <natcho9010@gmail.com>
2025-05-12 15:28:08 +0900

    chore: add mock data in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
+import type { Field } from "./components/Table";
+import { Table } from "./components/Table";
+
+const record: Field[] = [
+  { type: "text", label: "이름", required: true, value: "John Doe" },
+  {
+    type: "text",
+    label: "주소",
+    required: true,
+    value: "서울 강남구",
+  },
+  { type: "text", label: "메모", required: true, value: "외국인" },
+  {
+    type: "date",
+    label: "가입일",
+    required: true,
+    value: "2024-10-02",
+  },
+  { type: "text", label: "직업", required: true, value: "개발자" },
+  {
+    type: "checkbox",
+    label: "이메일 수신 동의",
+    required: true,
+    value: true,
+  },
+];
+
+const record2: Field[] = [
+  { type: "text", label: "이름", required: true, value: "Foo Bar" },
+  {
+    type: "text",
+    label: "주소",
+    required: true,
+    value: "서울 서초구",
+  },
+  { type: "text", label: "메모", required: true, value: "한국인" },
+  {
+    type: "date",
+    label: "가입일",
+    required: true,
+    value: "2024-10-01",
+  },
+  { type: "text", label: "직업", required: true, value: "PO" },
+  {
+    type: "checkbox",
+    label: "이메일 수신 동의",
+    required: true,
+    value: false,
+  },
+];
+
 export function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Table records={[record, record2]} />;
 }
