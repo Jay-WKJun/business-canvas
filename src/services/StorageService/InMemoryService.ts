30ca4b8c690f419f5b9d7bff8d3e95c4721d8fad
KyungJunWoo <natcho9010@gmail.com>
2025-05-14 15:17:47 +0900

    fix: InMemoryService methods

--- src/services/StorageService/InMemoryService.ts
+++ src/services/StorageService/InMemoryService.ts
@@ -5,18 +5,15 @@ export class InMemoryStorageService<
 > extends AbstractStorageService<T> {
   private storage: Record<string, T> = {};
 
-  get(key: string): T {
-    console.log("get", this.storage);
-    return this.storage[key];
+  get(key: string): T | null {
+    return this.storage[key] ?? null;
   }
 
   saveData(key: string, value: T): void {
     this.storage[key] = value;
-    console.log("saveData", this.storage);
   }
 
   remove(key: string): void {
     delete this.storage[key];
-    console.log("remove", this.storage);
   }
 }
