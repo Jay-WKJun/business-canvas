600124667427f6d7a47e271bcc0d2dbb02afa25d
KyungJunWoo <natcho9010@gmail.com>
2025-05-14 21:09:54 +0900

    feat: add strict option in Field & TableData zod

--- src/components/Table/models/Field.ts
+++ src/components/Table/models/Field.ts
@@ -2,12 +2,14 @@ import { z } from "zod";
 import dayjs from "dayjs";
 
 // 기본 필드 스키마 (공통 필드)
-export const FieldSchema = z.object({
-  type: z.enum(["text", "checkbox", "date", "select", "textarea"]),
-  label: z.string(),
-  required: z.boolean(),
-  schemaOrder: z.number(),
-});
+export const FieldSchema = z
+  .object({
+    type: z.enum(["text", "checkbox", "date", "select", "textarea"]),
+    label: z.string(),
+    required: z.boolean(),
+    schemaOrder: z.number(),
+  })
+  .strict();
 
 export type FieldSchemaType = z.infer<typeof FieldSchema>;
 export type FieldSchemaInputType = FieldSchemaType["type"];
@@ -17,26 +19,26 @@ export const Field = z.discriminatedUnion("type", [
   FieldSchema.extend({
     type: z.literal("checkbox"),
     value: z.boolean().default(false),
-  }),
+  }).strict(),
   FieldSchema.extend({
     type: z.enum(["date"]),
     value: z
       .string()
       .regex(/^\d{4}-\d{2}-\d{2}$/)
       .default(dayjs().format("YYYY-MM-DD")),
-  }),
+  }).strict(),
   FieldSchema.extend({
     type: z.enum(["select"]),
     value: z.string().default(""),
-  }),
+  }).strict(),
   FieldSchema.extend({
     type: z.enum(["text"]),
     value: z.string().max(20, "글자수 20자를 초과할 수 없습니다.").default(""),
-  }),
+  }).strict(),
   FieldSchema.extend({
     type: z.enum(["textarea"]),
     value: z.string().max(50, "글자수 50자를 초과할 수 없습니다.").default(""),
-  }),
+  }).strict(),
 ]);
 
 export type FieldType = z.infer<typeof Field>;
